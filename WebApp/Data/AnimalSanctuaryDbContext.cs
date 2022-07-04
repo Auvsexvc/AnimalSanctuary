daad436087fc517d5784f4943021770a626c3305
Auvsexvc <auvsexvc@gmail.com>
2022-07-04 19:42:33 +0200

    Add project files.

---
+++ WebApp/Data/AnimalSanctuaryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class AnimalSanctuaryDbContext : DbContext
+    {
+        public DbSet<Animal> Animals { get; set; }
+
+        public AnimalSanctuaryDbContext(DbContextOptions<AnimalSanctuaryDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ WebApp/Interfaces/IAnimalSanctuaryService.cs
@@ -0,0 +1,20 @@
+﻿using WebApp.Models;
+
+namespace WebApp.Interfaces
+{
+    public interface IAnimalSanctuaryService
+    {
+        int Create(Animal animal);
+
+        void Delete(int? id);
+
+        IEnumerable<Animal> GetAll();
+
+        Animal GetById(int? id);
+
+        bool IsEnoughSpace();
+
+        void Update(Animal animal);
+        int GetSpaceLeft();
+    }
+}
